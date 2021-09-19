e8c5e015160dd306e5b737b7bf8a6e80007090aa
devCynosure <sharanprakash.sfc@gmail.com>
2021-09-19 22:08:07 +0530

    Code to find integer max and min values

---
+++ DataSizeRange.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+
+class DataSizeRange{
+	public static void main(String[] args){
+		System.out.println("Minimum Integer Value is : "+Integer.MIN_VALUE);
+		System.out.println("Maximum Integer Value is : "+Integer.MAX_VALUE);
+	}
+}
