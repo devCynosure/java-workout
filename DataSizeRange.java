241e0d988218c86c749f102f87d5b7fbdaf9c965
devCynosure <sharanprakash.sfc@gmail.com>
2021-09-19 22:31:46 +0530

    Add bytes size

--- DataSizeRange.java
+++ DataSizeRange.java
@@ -2,8 +2,19 @@ import java.lang.*;
 
 class DataSizeRange{
 	public static void main(String[] args){
+		System.out.println("Find size of integer");
 		System.out.println("Minimum Integer Value is : "+Integer.MIN_VALUE);
 		System.out.println("Maximum Integer Value is : "+Integer.MAX_VALUE);
 		System.out.println("Integer Size In Byte is : "+ Integer.BYTES);
+
+		System.out.println("Find size of Bytes");
+		System.out.println("Minimum Byte value is : "+Byte.MIN_VALUE);
+		System.out.println("Maximum Byte value is : "+Byte.MAX_VALUE);
+		System.out.println("Byte Size is : "+ Byte.BYTES);
+
+
+		//System.out.println("Find size of String");
+		//System.out.println("Minimum String value is : "+String.MIN_VALUE);
+		//System.out.println("Maximum String value is : "+String.MAX_VALUE);
 	}
 }
