513a1b51108f3eac194fead77151789bd83206c3
devCynosure <sharanprakash.sfc@gmail.com>
2021-09-19 22:12:22 +0530

    Added code for finding integer size in bytes

--- DataSizeRange.java
+++ DataSizeRange.java
@@ -4,5 +4,6 @@ class DataSizeRange{
 	public static void main(String[] args){
 		System.out.println("Minimum Integer Value is : "+Integer.MIN_VALUE);
 		System.out.println("Maximum Integer Value is : "+Integer.MAX_VALUE);
+		System.out.println("Integer Size In Byte is : "+ Integer.BYTES);
 	}
 }
