39f0c40a7bdddb35149e94fab239ec2bdd347d0c
devCynosure <sharanprakash.sfc@gmail.com>
2021-09-20 22:48:43 +0530

    copy code and paste1

--- DataSizeRange.java
+++ DataSizeRange.java
@@ -13,6 +13,13 @@ class DataSizeRange{
 		System.out.println("Byte Size is : "+ Byte.BYTES);
 		//still check other also
 
+		System.out.println("Find size of integer");
+		System.out.println("Minimum Integer Value is : "+Integer.MIN_VALUE);
+		System.out.println("Maximum Integer Value is : "+Integer.MAX_VALUE);
+		System.out.println("Integer Size In Byte is : "+ Integer.BYTES);
+
+		
+
 		//System.out.println("Find size of String");
 		//System.out.println("Minimum String value is : "+String.MIN_VALUE);
 		//System.out.println("Maximum String value is : "+String.MAX_VALUE);
