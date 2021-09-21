1e92e352ab7904ba0d0b33647ff51c3ca7dc78ec
devCynosure <sharanprakash.sfc@gmail.com>
2021-09-21 23:22:23 +0530

    prompt added

--- stringReverse.java
+++ stringReverse.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class stringReverse {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
         String str;
         str = sc.nextLine();
         System.out.println("String : "+ str);
