dd8833446a681adc3a86d0f42e40f59ca897f997
devCynosure <sharanprakash.sfc@gmail.com>
2021-09-21 23:19:12 +0530

    Work on string reverse with no extra variable

---
+++ stringReverse.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class stringReverse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.nextLine();
+        System.out.println("String : "+ str);
+    }
+}
